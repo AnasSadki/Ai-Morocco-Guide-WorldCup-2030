2331a3f9dacc8d22206e132b750e6d8f86773c2f
AnasSadki <digitalmarketinghubproducts@gmail.com>
2025-06-30 17:17:26 +0200

    Add Msemen caption to food image

--- src/pages/FoodCulture.tsx
+++ src/pages/FoodCulture.tsx
@@ -35,6 +35,11 @@ const FoodCulture = () => {
                   <h3 className="text-lg font-semibold text-gray-900 text-center">Harira</h3>
                 </div>
               )}
+              {index === 1 && (
+                <div className="p-4 bg-white">
+                  <h3 className="text-lg font-semibold text-gray-900 text-center">Msemen</h3>
+                </div>
+              )}
             </div>
           ))}
         </div>
